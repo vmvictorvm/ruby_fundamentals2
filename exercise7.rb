7c47cf2567244a0d4e2cbb55cb6f94f5664bc019
Victor McLean <vmvictorvm@gmail.com>
2016-08-09 18:10:01 -0400

    Use the keys method to output

--- exercise7.rb
+++ exercise7.rb
@@ -8,8 +8,10 @@ def displayCohortInfo(students)
   students.each do |cohort, value|
     puts "#{cohort}: #{value} students"
   end
+  puts "\n"
 end
 
 displayCohortInfo(students)
 
 students[:cohort4] = 43
+puts students.keys
