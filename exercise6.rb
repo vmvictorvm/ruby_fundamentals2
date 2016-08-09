ede5fff4d4ac56d372343b9ddea7e05857f6f560
Victor McLean <vmvictorvm@gmail.com>
2016-08-09 17:27:08 -0400

    added display grocery method

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,12 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each do |item|
-  puts "* " + item
+
+def displayGrocery(grocery_list)
+  grocery_list.each do |item|
+    puts "* " + item
+  end
+  puts "\n"
 end
+
+displayGrocery(grocery_list)
+grocery_list << "rice"
+displayGrocery(grocery_list)
