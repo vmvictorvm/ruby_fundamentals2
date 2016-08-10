01a3b21f4a0241dd2aab0cbc06bba3d185e0dc0f
Victor McLean <vmvictorvm@gmail.com>
2016-08-10 09:47:47 -0400

    Classrooms expanded

--- exercise7.rb
+++ exercise7.rb
@@ -15,3 +15,13 @@ displayCohortInfo(students)
 
 students[:cohort4] = 43
 puts students.keys
+
+puts "\n"
+#Increase cohort size by 5%
+students.each do |cohort, value|
+  students[cohort] *= 1.05
+  students[cohort] = students[cohort].round
+end
+
+displayCohortInfo(students)
+puts "\n"
