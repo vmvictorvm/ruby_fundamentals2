d3af4947c1826f326835e9fe11050fc3c871965c
Victor McLean <vmvictorvm@gmail.com>
2016-08-10 10:30:31 -0400

    Bonus question + comments

--- exercise7.rb
+++ exercise7.rb
@@ -4,6 +4,8 @@ students = {
   :cohort3 => 22
 }
 
+# Create a method that displays the name and number of
+# students for each cohort
 def displayCohortInfo(students)
   students.each do |cohort, value|
     puts "#{cohort}: #{value} students"
@@ -13,7 +15,10 @@ end
 
 displayCohortInfo(students)
 
+#Add cohort 4 with 43 students
 students[:cohort4] = 43
+
+#Use the keys method to output all of the cohort names
 puts students.keys
 
 puts "\n"
@@ -29,3 +34,11 @@ puts "\n"
 #Delete the 2nd cohort and redisplay the hash
 students.delete(:cohort2)
 displayCohortInfo(students)
+
+# Bonus: Calculate the total number of students across all cohort
+# using each and a variable to keep track of the total.  Output the result
+total = 0
+students.each do |cohort, value|
+  total += value
+end
+puts "Total is #{total}"
